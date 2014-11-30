9c8441068b106d39f4300f5b8f39d2826d11c3aa
Mikkel Schmidt <msc@secoya.dk>
2014-12-01 00:18:11 +0100

    version 0.0.9

--- dist/capacitor.js
+++ dist/capacitor.js
@@ -1,5 +1,5 @@
 /**
- * @license capacitor.js 0.0.8 Copyright (c) 2014, Mikkel Schmidt. All Rights Reserved.
+ * @license capacitor.js 0.0.9 Copyright (c) 2014, Mikkel Schmidt. All Rights Reserved.
  * Available via the MIT license.
  */
 
